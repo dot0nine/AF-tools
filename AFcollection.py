48c6be69a85ab4cca7ab5991e882046ed4e280b2
Andrey V. Romanyuk <33423246+dot0nine@users.noreply.github.com>
2022-10-26 19:08:02 +0100

    1st fully working version with plotting

--- AFcollection.py
+++ AFcollection.py
@@ -15,6 +15,9 @@ class AF_Collection(object):
     def get_data(self):
         return self.models
 
+    def get_stat_par(self, x):
+        return [np.average(x), np.std(x), np.var(x), np.median(x)]
+
     def import_af_run(self, _dir):
         self.set_dir(_dir)
         f_extension = ".json"
@@ -22,21 +25,22 @@ class AF_Collection(object):
         # delimeters = ''
 
         subdirs = [x[0] for x in os.walk(self._dir)][1:]  #  exclude the root folder
-
+        for sd in subdirs:
+            print(sd)
         for sd in subdirs:
             model = sd.split('\\')[-1][:-7]
             print('Model\t', model)
-            self.models[model] = {'pLDDT': [], 'pAE': [], 'rank': [], 'model_num': []}  # to get rid of .result ending
+            self.models[model] = {'pLDDT': [], 'pAE': [], 'rank': [], 'model_num': [], 'ylim_pLDDT': [0, 100], 'ylim_pAE': [0,30]}  # to get rid of .result ending
 
             json_files = open_files(full_path=sd, _filter='*' + f_extension)
             for f in json_files:
                 if re.match(model + '_unrelaxed_rank_\d+_model_\d+_scores', f):
-                    print('File', f)
+                    # print('File', f)
 
                     pLDDT, pAE = self.scores(f'{sd}\\{f}')
                     # print()
                     rank_n, model_n = self.extract_rank_model_n(f, model)
-                    print('Pars\t', (pLDDT, pAE, rank_n, model_n))
+                    # print('Pars\t', (pLDDT, pAE, rank_n, model_n))
                     for k, v in zip(('pLDDT', 'pAE', 'rank', 'model_num'), (pLDDT, pAE, rank_n, model_n)):
                         self.models[model][k].append(v)
 
@@ -45,6 +49,22 @@ class AF_Collection(object):
             self.models[model]['pLDDT_best'] = max(self.models[model]['pLDDT'])
             self.models[model]['pAE_av'] = np.mean(self.models[model]['pAE'])
             self.models[model]['pAE_best'] = min(self.models[model]['pAE'])
+
+    # def output_analysis(self, model):
+    #     data = self.get_data()[model]
+    #     pars = ['pLDDT', 'pAE']
+    #     par_dict = {}
+    #     for p, yl in zip(pars, [(0, 100), (0, 30)]):
+    #         par_dict[p] = {'dataset': data[p], 'ylim': yl, f'{p}_best': data[ f'{p}_best']}
+    #         self.models[model] {'dataset': data[p], 'ylim': yl, f'{p}_best': data[ f'{p}_best']}
+    #         for k, v in  zip(['av', 'std', 'var', 'med'], self.get_stat_par(data[p])):
+    #             par_dict[p][k] = v
+    #
+    #     for k, v in zip(('ylim_pLDDT', 'ylim_pAE','av', 'std', 'var', 'med'), ()):
+
+
+        # return par_dict
+
 
 
 
--- test.py
+++ test.py
@@ -1,11 +1,73 @@
 import AFcollection
-import seaborn as sn
-
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+import pandas as pd
 scapCC4 = AFcollection.AF_Collection()
 
-scapCC4.import_af_run(r'C:\Users\lj21342\OneDrive - University of Bristol\Documents\01-Science\Computational\scapCC4\AF2\SC')
-print(scapCC4.get_data())
+scapCC4.import_af_run(r'C:\Users\lj21342\OneDrive - University of Bristol\Documents\01-Science\Computational\scapCC4\AF2\apCC4_x4 vs SC\\')
+data_dict = scapCC4.get_data()
+models = pd.DataFrame.from_dict(data_dict, orient='index')
+# data_dict = {}
+# for model in models:
+#     data_dict[model] = scapCC4.output_analysis(model)
+# data = pd.DataFrame.from_dict(data_dict)
+print(models.columns)
+fig1, axs = plt.subplots(1, 2)  # pLDDT and pAE
+# sns.set_theme(style="ticks", palette="pastel")
+axs = np.array(axs)  # https://www.pythonfixing.com/2021/10/fixed-python-subplot-used-to-show-one.html
+fig1.suptitle('AF2 output', fontsize=16)
+pars = ['pLDDT', 'pAE']
+# par_dict = {}
+# for p in pars:
+#     par_dict = {'dataset':[], 'ylim':[], 'av': 0, 'std': 0, 'var': 0, 'med': 0}
+
+# out_str = 'Protein\tpLDDT_av\tpLDDT_best\tpAE_av\tpAE_best\n'
+
+for par, ax, yl in zip(pars, axs.flat, ((0, 100), (0, 30))):  # plot only the parameters from the list specified above
+    ax.set_title(par, fontsize=14)
+    # out_str += par + '\n'
+    palette = itertools.cycle(sns.color_palette("Set2"))
+    # for model in models:
+    # data = scapCC4.output_analysis(model)
+    #     ax.scatter()
+
+    # print([scapCC4.output_analysis(model)[par]['dataset'] for model in models])
+    # sns.boxplot(x=models.keys(), y=[scapCC4.output_analysis(model)[par]['dataset'] for model in models])
+    # for x, y in zip(models.keys(), [scapCC4.output_analysis(model)[par]['dataset'] for model in models]):
+    #     sns.barplot(x=x, y=y)
+    # print(len(models['pLDDT_av']))
+    # print(models.iloc[0])
+    # print(models.iloc[0])
+    # print(models['pLDDT_av'])
+    # print(models[f'{par}'].index)
+    # print(models[f'{par}'].values)
+    sns.boxplot(models[f'{par}'], ax=ax)
+    # print(data_dict.keys())
+    # print([data_dict[model][f'{par}'] for model in data_dict.keys()])
+    # for model in data_dict.keys():
+    #     sns.boxplot(x=data_dict.keys(), y=data_dict[model][f'{par}'], ax=ax)
 
 
+    # sns.boxplot(x=models.iloc[0], y=models['pLDDT_av']) #, data=models)
+    #ax.set_xtick(rotation=45)
+
+        # for p in pars:
+        # # pLDDT, pLDDT_av, pLDDT_best, pAE, pAE_av, pAE_best = [x for x in models[model].values()]
+        # for k,v in zip([pLDDT, pLDDT_av, pLDDT_best, pAE, pAE_av, pAE_best])models[model].items():
+        #     data = samples[s].get_surfaces()[par]['dataset']
+        #     pLDDT_av, pLDDT_best, pAE_av, pAE_best = samples[s].get_stat_par(data)
+        #     color = next(palette)
+        #     out_str += f'{s}\t{pLDDT_av:.1f}\t{pLDDT_best:.1f}\t{pAE_av:.2f}\t{pAE_best:.2f}\n'
+        #     label = f'{s}'
+        #     sns.histplot(ax=ax, data=data, bins=101, binrange=samples[s].get_surfaces()[par]['xlim'], kde=True,
+        #                  alpha=0.7, color=color, common_norm=False, line_kws={"lw": 3}, label=label, stat=stat)
+        # out_str += '\n'
+        # ax.set_xlim(samples[s].get_surfaces()[par]['xlim'])
+
+    ax.set_ylim(yl)
+
+
+plt.show()
 
